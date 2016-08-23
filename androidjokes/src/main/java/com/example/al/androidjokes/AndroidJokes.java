5e3ff8e4fbdda81b3b78482d99d909a5dd7cacf2
Alz-Android <apiran06@gmail.com>
2016-08-22 21:39:02 -0700

    Initial commit

---
+++ androidjokes/src/main/java/com/example/al/androidjokes/AndroidJokes.java
@@ -0,0 +1,45 @@
+package com.example.al.androidjokes;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.TextView;
+
+public class AndroidJokes extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_android_jokes);
+
+        Intent intent = getIntent();
+        String joke = intent.getStringExtra("jokeExtra");
+        displayJoke(joke);
+    }
+
+
+    public void displayJoke(String joke){
+
+        TextView textViewJoke = (TextView) findViewById(R.id.jokeBox);
+        textViewJoke.setText(joke);
+
+    }
+
+//    @Override
+//    public View onCreateView(View parent, String name, Context context, AttributeSet attrs) {
+//         //super.onCreateView(parent, name, context, attrs);
+//
+//
+//
+//        Intent intent = getIntent();
+//        String joke = intent.getStringExtra("jokeExtra");
+//
+//        View textViewJoke = (TextView) findViewById(R.id.jokeBox);
+//        textViewJoke. setText(joke);
+//
+//        return textViewJoke;
+//    }
+}
---
+++ jokewizard/src/main/java/com/example/JokeWizard.java
@@ -0,0 +1,11 @@
+package com.example;
+
+public class JokeWizard {
+    public JokeWizard() {
+    }
+
+    public String getJoke() {
+
+        return("new joke");
+    }
+}
